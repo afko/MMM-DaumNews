f974987ecf33e0ca270462dd142f370af8797a25
Sungje Kim <sungje.kim@one-line.com>
2019-03-24 13:50:26 +0900

    Test004 - txt를 json으로 바꿔서

--- MMM-DaumNews.js
+++ MMM-DaumNews.js
@@ -7,38 +7,37 @@
 
 Module.register('MMM-DaumNews', {
     defaults: {
-        text: "Hi"
+        text: "Hi",
+        updateInterval: 30 * 60 * 1000
     },
-    start: function () {
-        var self = this;
-        setInterval(function () {
-            self.updateDom(); // no speed defined, so it updates instantly.
-        }, 1000); //perform every 1000 milliseconds.
+
+    start: function(){
+		this.sendSocketNotification("START", this.config);
     },
+
+    socketNotificationReceived: function(notification, payload) {
+		if(notification === "DATA"){
+			this.dataFile = payload;
+			this.updateDom();
+		}
+	},
 
     getScripts: function () {
         return [
-            '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-            'news_test.js'
+            '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
         ];
     },
     getStyles: function () {
         return ["font-awesome.css"];
     },
 
-    getDom: function () {
-        var wrapper = document.createElement("div");
-        wrapper.id = "news";
-        wrapper.innerHTML = this.config.text;
-
-        var symbol =  document.createElement("span");
-        symbol.className = "fa fa-home";
-        wrapper.appendChild(symbol);
-
-        getNews();
-        console.log("There is no problem");
-        Log.error('error');
-        
-        return wrapper;
-    }
+    getDom: function(){
+		var wrapper = document.createElement("div");
+		if(this.dataFile){
+			wrapper.innerHTML = this.dataFile;
+		} else {
+			wrapper.innerHTML = "No data";
+		}
+		return wrapper;
+	}
 });
